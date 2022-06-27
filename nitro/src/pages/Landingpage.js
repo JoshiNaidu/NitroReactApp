3efd60142bc73ade5609bbe4d26471f5fa8037c5
JoshiNaidu <94054346+JoshiNaidu@users.noreply.github.com>
2022-06-27 14:51:10 +0530

    first commit

---
+++ nitro/src/pages/Landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../components/Footer'
+function Landingpage() {
+  return (
+    <div>
+
+        <Footer/>
+
+    </div>
+  )
+}
+
+export default Landingpage
